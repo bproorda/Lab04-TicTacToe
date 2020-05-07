7e5312b91dc43de9e7f6b3f08f0ace2e744e8e57
Brennan Roorda <54748984+bproorda@users.noreply.github.com>
2020-05-07 17:16:40 -0500

    did some setup

---
+++ Lab04_TicTacToe/Classes/Board.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lab04_TicTacToe.Classes
+{
+    class Board
+    {
+		/// <summary>
+		/// Tic Tac Toe Gameboard states
+		/// </summary>
+		public string[,] GameBoard = new string[,]
+		{
+			{"1", "2", "3"},
+			{"4", "5", "6"},
+			{"7", "8", "9"},
+		};
+
+
+		public  void DisplayBoard()
+		{
+
+			//TODO: Output the board to the console
+			
+			Console.WriteLine(string.Join(", ", this));
+			
+			
+		}
+	}
+}
---
+++ Lab04_TicTacToe/Program.cs
@@ -0,0 +1,29 @@
+﻿using Lab04_TicTacToe.Classes;
+using System;
+
+namespace Lab04_TicTacToe
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            StartGame();
+        }
+
+        static void StartGame()
+        {
+            // TODO: Setup your game. Create a new method that creates your players and instantiates the game class. Call that method in your Main method.
+            // You are requesting a Winner to be returned, Determine who the winner is output the celebratory message to the correct player. If it's a draw, tell them that there is no winner. 
+            Player player1 = new Player();
+            Player player2 = new Player();
+            player1.Name = "Scott";
+            player2.Name = "Brennan";
+            Game newGame = new Game(player1, player2);
+
+            //newGame.Board.DisplayBoard;
+        }
+
+
+    }
+}
